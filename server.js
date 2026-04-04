45ff9ddd453c952a36fa8ae08e341d083b1c9719
pedro2506 <59938479+pedro2506@users.noreply.github.com>
2026-04-04 16:55:27 +0000

    up11

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const mysql = require("mysql2");
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static("public"));
 
 // 🔌 Conexão com MySQL
