7066a0b8e8ce9d1e4961a48e649c6ae079d2fd13
pedro2506 <59938479+pedro2506@users.noreply.github.com>
2026-04-07 19:56:34 +0000

    up40

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.delete("/produtos/:id", autenticar, (req, res) => {
 
 app.use(express.static("public"));
 
-app.use((req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
