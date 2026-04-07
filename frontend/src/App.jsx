3a80334c4977a1ad11930145b569c15be845144f
pedro2506 <59938479+pedro2506@users.noreply.github.com>
2026-04-07 18:41:11 +0000

    up30

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Sistema de Produtos</h1>;
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [token, setToken] = useState("");
+
+  const login = async () => {
+    const res = await fetch("https://exercicio-express.onrender.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, senha })
+    });
+
+    const data = await res.json();
+
+    if (data.token) {
+      setToken(data.token);
+      alert("Login feito!");
+    } else {
+      alert("Erro no login");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Senha"
+        type="password"
+        onChange={(e) => setSenha(e.target.value)}
+      />
+
+      <button onClick={login}>Entrar</button>
+
+      {token && <p>Logado ✅</p>}
+    </div>
+  );
 }
 
 export default App;
